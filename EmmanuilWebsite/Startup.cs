cc6d6c0b82bbbb0aadbc53fe3f12af7489506653
sozda <sozda@IVANTKAPC>
2022-08-20 12:14:02 +0700

    Add project files.

---
+++ EmmanuilWebsite/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using EmmanuilWebsite.Service;
+
+namespace EmmanuilWebsite
+{
+	public class Startup
+	{
+		public IConfiguration Configuration { get; }
+		public Startup(IConfiguration configuration) => Configuration = configuration;
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			//подключение конфига из appsettings.json
+			Configuration.Bind("Project", new Config());
+
+			//добавление поддержки контроллеров и представлений(MVC)
+			services.AddControllersWithViews()
+				.SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Latest)
+				.AddSessionStateTempDataProvider();
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			//отчеты ошибок
+			if(env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseRouting();
+
+			//поддержка css, js, и др.
+			app.UseStaticFiles();
+
+			//добавление маршрутов
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
+}
